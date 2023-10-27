2970e4df02c448a1f98633a5c1ee1b1f9bd53687
uddinr <rehanuddinuk@gmail.com>
2023-10-27 17:50:43 +0100

    Initial Commit

---
+++ MovieVault/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieVault
+//
+//  Created by Rehan Uddin on 27/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MovieVaultUITests/MovieVaultUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MovieVaultUITestsLaunchTests.swift
+//  MovieVaultUITests
+//
+//  Created by Rehan Uddin on 27/10/2023.
+//
+
+import XCTest
+
+final class MovieVaultUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
